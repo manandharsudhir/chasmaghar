a02b0df85992c1928685b3a12d76e0412a3ffed3
sudhir manandhar <manandharsudhir@gmail.com>
2023-03-07 19:19:38 +0545

    detail page done

--- app.py
+++ app.py
@@ -6,5 +6,14 @@ app = Flask(__name__)
 def home():
     return render_template("index.html")
 
+@app.route("/detail")
+def detail():
+    return render_template("detail.html")
+
+
+@app.route("/checkout")
+def checkout():
+    return render_template("checkoutform.html")
+
 if __name__=="__main__":
     app.run(debug=True)
