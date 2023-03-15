bdef243ca91a78e132ed2cea2bb0486ce9a0c482
sudhir manandhar <manandharsudhir@gmail.com>
2023-03-15 21:59:03 +0545

    added project

--- app.py
+++ app.py
@@ -3,10 +3,18 @@ from addproduct import AddproductForm
 from checkout import CheckoutForm
 from flask_sqlalchemy import SQLAlchemy
 import os
+import uuid
+from login import LoginForm
+from flask_login import LoginManager,UserMixin, current_user,login_user,logout_user,login_required
+from flask_bcrypt import Bcrypt
 
 app = Flask(__name__)
 app.config['SECRET_KEY']="de9e5b220476ba0aba47040eb9b2fea9"
 app.config['SQLALCHEMY_DATABASE_URI']='sqlite:///chasmaghar.db'
+bcrypt= Bcrypt(app)
+login_mananger = LoginManager(app)
+login_mananger.login_view = 'adminlogin'
+login_mananger.login_message_category="info"
 
 db=SQLAlchemy(app)
 
@@ -37,6 +45,15 @@ class Order(db.Model):
     def __repr__(self):
         return f"Order({self.id}{self.firstname},{self.email},{self.phone},{self.product})"
 
+class User(db.Model,UserMixin):
+    id = db.Column(db.Integer,primary_key=True)
+    firstname = db.Column(db.String(20),nullable=False)
+    lastname = db.Column(db.String(20),nullable=False)
+    email = db.Column(db.String(50),nullable=False)
+    password = db.Column(db.String(50),nullable=False)
+    isSuperAdmin = db.Column(db.Boolean,default=False)
+    def __repr__(self):
+        return f"Order({self.id}{self.firstname},{self.email},{self.phone},{self.product})"
 
 
 @app.route("/")
@@ -69,20 +86,22 @@ def checkout(id):
 
 def saveProductImage(form_picture,file_name):
     print("product image")
-    _,f_ext=os.path.splitext(form_picture.filename)    
-    picture=file_name+f_ext
+    _,f_ext=os.path.splitext(form_picture.filename)  
+           
+    random_id = uuid.uuid4()  
+    picture=random_id+f_ext
     picture_path = os.path.join(app.root_path,"static/images/products",picture)
     form_picture.save(picture_path)
     return f"../static/images/products/{picture}"
 
 @app.route("/addproduct",methods=['GET','POST'])
+@login_required
 def addproduct():
     products = Product.query.all()
     print(products)
     form=AddproductForm()
     if form.validate_on_submit():
-        if form.productImage.data:
-            print("k ho k ho")
+        if form.productImage.data:  
             picture_file = saveProductImage(form.productImage.data,request.form["name"])
             flash("Product Added Successful",category="success")
             product = Product(name = request.form["name"],detail=request.form["description"],price=request.form["price"],discounted_price=request.form["discountPrice"],has_discount=form.checkbox.data,images=picture_file)
@@ -94,6 +113,7 @@ def addproduct():
     return render_template("addproductform.html",form=form,products=products)
 
 @app.route("/viewallorders")
+@login_required
 def viewAllOrders():
     
     orders = Order.query.all()
@@ -101,6 +121,7 @@ def viewAllOrders():
     return render_template("viewallorders.html",orders=orders)
 
 @app.route("/adminviewproduct")
+@login_required
 def adminViewProducts():
     products = Product.query.all()
     print(products)
@@ -110,12 +131,37 @@ def adminViewProducts():
 def delete(id):
     print(id)
     product = Product.query.filter_by(id=id).first()
-    print(product)
+    file_path_str = product.images.replace('../', '')
+    if os.path.exists(file_path_str):
+        os.remove(file_path_str)    
     db.session.delete(product)
     db.session.commit()
     return redirect(url_for('adminViewProducts'))
 
+@app.route("/adminlogin",methods=['GET','POST'])
+def adminlogin():
+    if current_user.is_authenticated:
+        return redirect(url_for('adminViewProducts'))
+    else:
+        form=LoginForm()
+        if form.validate_on_submit():
+            user = User.query.filter_by(email=form.email.data).first()
+            if user and bcrypt.check_password_hash(user.password,form.password.data):
+                login_user(user,remember = True)
+                return redirect(url_for('adminViewProducts'))
+            else:
+                flash('Invalid Credientials',category="danger")
+    return render_template("adminlogin.html",title = "Login",form=form)
 
+
+@app.route("/logout")
+def logout():
+    logout_user()
+    return redirect(url_for('adminlogin'))
+
+@login_mananger.user_loader
+def load_user(user_id):
+    return User.query.get(int(user_id))
 
 if __name__=="__main__":
     app.run(debug=True)
